ccd69606e6958d78b32b03710c742fbdef03862e
YCX-C <ycx974577817@gmail.com>
2024-09-18 14:34:59 +0800

    add koin-vm

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -126,6 +126,7 @@ dependencies {
 
     implementation(libs.koin.core)
     implementation(libs.koin.android)
+    implementation(libs.koin.vm)
     implementation(libs.koin.compose)
 
     implementation(libs.initializer)
