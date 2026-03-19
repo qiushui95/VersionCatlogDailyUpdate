6777bc99444eabc8c595de89f97ddcb29280876b
YCX-C <ycx974577817@gmail.com>
2026-03-19 10:59:55 +0800

    接入Foojay

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,12 +40,6 @@ android {
     buildFeatures {
         compose = true
     }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-
 }
 
 dependencies {
